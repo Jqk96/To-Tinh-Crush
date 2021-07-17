2d75b895f5689642de5defa5464e452f4e30967f
Jqk96 <87538960+Jqk96@users.noreply.github.com>
2021-07-17 08:59:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Ơ kìa Ngọc!',
-    introDesc: `A tưởng công chúa thì chỉ ngủ trong rừng thôi
-    Sao bạn lại ở đây thế này`,
-    btnIntro: 'haha',
+    introDesc: 'A tưởng công chúa thì chỉ ngủ trong rừng thôi, 
+    'Sao bạn lại ở đây thế này',
+    btnIntro: 'Haha',
     title: 'Nếu bạn là ngôi sao sáng nhất trên bầu trời có trăm nghìn vì tinh tú 😙',
-    desc: 'Thì A chỉ muốn được làm thằng nhóc chăn cừu ngồi trên mỏm đá nhỏ của địa cầu ngắm nhìn bầu trời ấy mà si tình thổi tiêu ',
+    desc: 'Thì A chỉ muốn được làm thằng nhóc chăn cừu ngồi trên mỏm đá nhỏ của địa cầu ngắm nhìn bầu trời ấy mà si tình thổi tiêu',
     btnYes: 'Thích anh <33',
     btnNo: 'Không thích anh :3',
     question:'Bạn có tin vào sự chân thành của A không <3',
     btnReply: 'Hãy trả lời thật lòng <3',
     reply: 'Em có <33333333',
-    mess: 'Haha, chắc chắn rồi bạn ạ 😘😘',
+    mess: 'Haha chắc chắn rồi bạn ạ 😘😘',
     messDesc: 'Vì thương em là điều A không thể ngờ.',
     btnAccept: '<3<3<3',
-    messLink: 'http://fb.com/senh.chenh.18' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/senh.chenh.18'
 }
